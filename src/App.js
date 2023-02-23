f81171113ca3636023585b56d3c5e249f981d313
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-02-23 01:53:25 +0100

    nextCard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./components/Card/Card";
+import logo from "./assets/react.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img className="title-logo" src={logo} alt="" />
       </header>
+
+      <div className="table-container">
+        <div className="table">
+          <h2 className="table-title">Languages</h2>
+          <Card />
+        </div>
+      </div>
     </div>
   );
 }
